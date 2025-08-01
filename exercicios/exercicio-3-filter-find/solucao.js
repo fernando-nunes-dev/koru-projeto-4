6a2c32ca4386b92d21cbe09f65ab283f308463ca
Fernando Nunes <fernandonunes198326@gmail.com>
2025-07-31 22:45:32 -0300

    feat: exercicio 3-b filter find

--- exercicios/exercicio-3-filter-find/solucao.js
+++ exercicios/exercicio-3-filter-find/solucao.js
@@ -2,3 +2,8 @@
 const inactiveStudents = students.filter((student) => !student.active);
 
 console.log("Desafio 3-a (Estudantes inativos):", inactiveStudents);
+
+// Solução do desafio 3-b
+const studentById = students.find((student) => student.id === 3);
+
+console.log("Desafio 3-b (Estudante com id 3):", studentById);
