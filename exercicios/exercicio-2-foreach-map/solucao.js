10d39206580e69222ea6f4740556f204c7aec68b
Fernando Nunes <fernandonunes198326@gmail.com>
2025-07-31 21:48:18 -0300

    feat: exercício 2-a foreach map

---
+++ exercicios/exercicio-2-foreach-map/solucao.js
@@ -0,0 +1,29 @@
+const products = [
+  { name: "Camiseta", category: "Roupas", price: 29.99 },
+  { name: "Celular", category: "Eletrônicos", price: 999.99 },
+  { name: "Livro", category: "Livros", price: 49.99 }
+];
+
+// Solução do desafio 2-a
+const productStrings = products.map((product) => {
+  return `${product.name} - ${product.category}`;
+});
+
+console.log("Desafio 2-a:", productStrings);
+
+// Solução do desafio 2-b
+const pricesWithTax = products.map((product) => {
+  const priceWithTax = product.price * 1.15;
+  return parseFloat(priceWithTax.toFixed(2));
+});
+
+console.log("Desafio 2-b:", pricesWithTax);
+
+// Solução do desafio 2-c
+let totalValue = 0;
+
+products.forEach((product) => {
+  totalValue += product.price;
+});
+
+console.log("Desafio 2-c (Valor Total):", totalValue.toFixed(2));
