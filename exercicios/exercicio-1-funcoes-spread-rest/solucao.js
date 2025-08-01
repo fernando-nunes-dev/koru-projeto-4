3cdb401dbc3c8d92c9f83387c12f824afbf86111
Fernando Nunes <fernandonunes198326@gmail.com>
2025-07-31 21:14:57 -0300

    Resolução do exercicio 1.b funções spread rest

--- exercicios/exercicio-1-funcoes-spread-rest/solucao.js
+++ exercicios/exercicio-1-funcoes-spread-rest/solucao.js
@@ -11,3 +11,15 @@ const addIsAdultProperty = (personObject) => {
 
 console.log("Desafio 1-a (Fernando):", addIsAdultProperty(person));
 console.log("Desafio 1-a (Anna):", addIsAdultProperty(person2));
+
+// Solução do desafio 1-b
+const fruits = ["maçã", "banana"];
+const moreFruits = ["laranja", "uva"];
+const evenMoreFruits = ["kiwi", "abacaxi"];
+
+const combineArrays = (...arrays) => {
+  return [].concat(...arrays);
+};
+
+const combined = combineArrays(fruits, moreFruits, evenMoreFruits);
+console.log("Desafio 1-b:", combined);
