e9efe38bfbc67c89fe5ad0216fb521faec01ff64
Fernando Nunes <fernandonunes198326@gmail.com>
2025-07-31 22:51:38 -0300

    feat: exercicio 3-c filter find

--- exercicios/exercicio-3-filter-find/solucao.js
+++ exercicios/exercicio-3-filter-find/solucao.js
@@ -1,3 +1,11 @@
+const students = [
+{ id: 1, name: "Lucas", age: 22, grades: [88, 76, 91], active: true },
+  { id: 2, name: "Juliana", age: 20, grades: [95, 89, 93], active: true },
+  { id: 3, name: "Rafael", age: 19, grades: [65, 72, 70], active: false },
+  { id: 4, name: "Camila", age: 21, grades: [78, 85, 80], active: true },
+  { id: 5, name: "Fernando", age: 23, grades: [92, 94, 90], active: false },
+];
+
 // Solução do desafio 3-a
 const inactiveStudents = students.filter((student) => !student.active);
 
@@ -7,3 +15,10 @@ console.log("Desafio 3-a (Estudantes inativos):", inactiveStudents);
 const studentById = students.find((student) => student.id === 3);
 
 console.log("Desafio 3-b (Estudante com id 3):", studentById);
+
+// Solução do desafio 3-c
+const studentsWithLowGrade = students.filter((student) => {
+  return student.grades.some((grade) => grade < 70);
+});
+
+console.log("Desafio 3-c (Alunos com nota < 70):", studentsWithLowGrade);
